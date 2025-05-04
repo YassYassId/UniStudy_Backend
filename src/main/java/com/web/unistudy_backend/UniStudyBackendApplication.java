f02e467eb7db93d3877f7c8f259d46df29197f70
YassYassId <idrissiy371@gmail.com>
2025-05-04 16:23:35 +0100

    First commit

---
+++ src/main/java/com/web/unistudy_backend/UniStudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.web.unistudy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniStudyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniStudyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/unistudy_backend/UniStudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.unistudy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniStudyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
